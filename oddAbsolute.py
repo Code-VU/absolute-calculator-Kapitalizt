9031733036be7fcdb924e95f122e292daae6db53
Rob Kirchoff <Rob.Kirchoff@vu.com>
2022-04-12 14:19:22 -0500

    1st

--- oddAbsolute.py
+++ oddAbsolute.py
@@ -1,8 +1,23 @@
 def calculateAbsolute():
     
     # This first line is provided for you
-    in_num  = input("Enter a number: ")
+    try:
+        in_num  = float(input("Enter a number: "))
+    except:
+        print("Please enter a number.")
+        print("---Terminating Program---")
+        quit()
+
+    if in_num == 21:
+        print("Result: 0")
+    elif in_num < 21:
+        print(21 - in_num)
+    elif in_num > 21:
+        print(2*(in_num-21))
     # end assignment
+
+
+
 
 ## if you want to test locally before you try to sync
 ## uncomment calculateAbsolute() and run > python payCalculator.py
